8b19dcaf886cb7e7111e20cc90b16b018d121ea2
Fadonougbo <fadougbogautier@gmail.com>
2023-05-01 10:52:54 +0100

    correction bug  du loader

--- public/composants/Header/Header.jsx
+++ public/composants/Header/Header.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable @babel/object-curly-spacing */
 /* eslint-disable react/display-name */
 /* eslint-disable react/self-closing-comp */
-import React from "react"
+import React, { useEffect} from "react"
 import { Helper } from "../../js/Helper.js";
 
 export const Header=({mainRef})=>
 {
+
 
     const menuClick=()=>{
         //burger animation
@@ -15,6 +16,15 @@ export const Header=({mainRef})=>
         //Main translate function
         Helper.translateMain(mainRef.current)
     }
+
+    useEffect(()=>{
+
+        window.addEventListener("load",()=>{
+            let wrapperMenu = document.querySelector('.wrapper-menu');
+           wrapperMenu.style.setProperty("display","flex")
+        })
+    },[])
+
     return (
             <header>
                 <div onClick={menuClick} id="burger">
--- public/js/loader.js
+++ public/js/loader.js
@@ -1,10 +1,13 @@
 
 export const loadAnimation=()=>{
 
+    document.body.style.setProperty("overflow","hidden")
     const onLoad=()=>{
+
         const loader=document.querySelector("#loader")
         loader.classList.remove("load")
         loader.style.display="none"
+        document.body.style.setProperty("overflow","")
     }
     
     window.addEventListener("load",onLoad)
